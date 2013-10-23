1cede29a2f212fc53bc57a3a9c0deafa03d42437
Ahmed Alhafidh <ahmedalhafidh@gmail.com>
2013-10-23 11:08:44 -0700

    Assignment 1

---
+++ Assignment_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n\n";
+   cout << "My family name is Alhafidh." << endl;
+   cout << "My student ID is 20179114." << endl;
+   cout << "According to the syllabus, the final quiz will take place "
+      << "in Week 12." << endl << endl;
+   return 0;
+}
+
+/* ---------------- RUN ----------------
+
+Hello World!
+
+My family name is Alhafidh.
+My student ID is 20179114.
+According to the syllabus, the final quiz will take place in Week 12.
+
+Press any key to continue . . .
+
+----------------------------------------*/
